24226632d836c83130fa328a98e0e0a5dbc676fa
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-19 20:28:26 +0200

    update config to acore

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 # Connection to the world database (address, username, password, database)
-$db = new mysqli("localhost","root","password","world");
+$db = new mysqli("localhost","root","root","acore_world");
 
 if (mysqli_connect_error())
   die('Connect Error (' . mysqli_connect_errno() . ') '
